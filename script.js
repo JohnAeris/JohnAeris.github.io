7308370eff4155898cf7ccc4ac7ec5f5bf56328a
John Aeris <barrotjohnaeris@gmail.com>
2022-12-26 23:01:03 +0800

    add responsive menu button function

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const header = document.querySelector("header")
 
+/*sticky scroll*/
 window.addEventListener("scroll", function() {
 	header.classList.toggle("sticky", window.scrollY > 0)
 });
 
+/*sticky scroll style*/
 document.addEventListener("scroll", ()=> {
 	if (window.scrollY > 1000) {
 		header.classList.add("scrolled");
 	} else {
 		header.classList.remove("scrolled")
 	}
-})
+});
+
+/*responsive menu*/
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('active')
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('active')
+};
